4b319caf61191e60c6bb6c74042838b70b942f25
wjdrlgh93 <mario115@naver.com>
2025-06-16 16:32:12 +0900

    jgh

---
+++ src/pages/tmp/JGH_tmp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JGH_tmp = () => {
+  return (
+    <div>JGH_tmp</div>
+  )
+}
+
+export default JGH_tmp
--- src/router/root.jsx
+++ src/router/root.jsx
@@ -1,23 +1,20 @@
 import React, { lazy, Suspense } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
-import toShopRouter from './toShopRouter'
 
 
 const Loading = <div className='loading'>Loading...</div>
 
 const IndexPage = lazy(() => import(`../pages/index/IndexPage`))
-const JGHPage = lazy(() => import(`../pages/tmp/JGH_tmp`))
-
+const JPage = lazy(() => import('../pages/tmp/JGH_tmp'))
 
 const root = createBrowserRouter([
     {
         path: '',
         element: <Suspense fallback={Loading}><IndexPage /></Suspense>
-    }
-    // 임시 링킹 path
+    },
     {
-        path: '/JGH_tmp',
-        element: <Suspense fallback={Loading}><JGHPage /></Suspense>
+        path: '/JGHtmp',
+        element: <Suspense fallback={Loading}><JPage /></Suspense>
     }
 ])
 
