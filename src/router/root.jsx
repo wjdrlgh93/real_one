3f414093a589af6a17ce5fdee0b10849adde3e77
wjdrlgh93 <mario115@naver.com>
2025-06-16 16:06:32 +0900

    first

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -6,6 +6,7 @@ import toShopRouter from './toShopRouter'
 const Loading = <div className='loading'>Loading...</div>
 
 const IndexPage = lazy(() => import(`../pages/index/IndexPage`))
+const JGHPage = lazy(() => import(`../pages/tmp/JGH_tmp`))
 
 
 const root = createBrowserRouter([
@@ -16,7 +17,7 @@ const root = createBrowserRouter([
     // 임시 링킹 path
     {
         path: '/JGH_tmp',
-        element:<
+        element: <Suspense fallback={Loading}><JGHPage /></Suspense>
     }
 ])
 
