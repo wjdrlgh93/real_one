266058b2c61ea0b0effa4bbf2287220e8ad0cf13
r2dsquad <redface0212@naver.com>
2025-06-16 16:25:16 +0900

    test

---
+++ src/pages/tmp/test1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const test1 = () => {
+  return (
+    <div>
+      te
+    </div>
+  )
+}
+
+export default test1
--- src/router/root.jsx
+++ src/router/root.jsx
@@ -1,10 +1,10 @@
 import React, { lazy, Suspense } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
-import toShopRouter from './toShopRouter'
+
 
 
 const Loading = <div className='loading'>Loading...</div>
-
+const ShopPage = lazy(()=> import(`../pages/tmp/test1.jsx`))
 const IndexPage = lazy(() => import(`../pages/index/IndexPage`))
 
 
@@ -12,12 +12,14 @@ const root = createBrowserRouter([
     {
         path: '',
         element: <Suspense fallback={Loading}><IndexPage /></Suspense>
-    }
+    },
     // 임시 링킹 path
+
     {
-        path: '/JGH_tmp',
-        element:<
+        path: '/tets2',
+        element:<Suspense fallback={Loading}><ShopPage/></Suspense>
     }
+
 ])
 
 
