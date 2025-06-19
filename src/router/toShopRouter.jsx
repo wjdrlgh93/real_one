4a98c975fd4a435ae58e65ac08fcce2707f2e1f5
tmddn08315@gmail.com <tmddn08315@gmail.com>
2025-06-19 17:39:17 +0900

    0619

---
+++ src/router/toShopRouter.jsx
@@ -0,0 +1,68 @@
+import React, { lazy, Suspense } from 'react'
+import { Navigate } from 'react-router-dom'
+import toToyRouter from './toToyRouter'
+import toBathRouter from './toBathRouter'
+
+const Loading = <div className='loading'>Loading...</div>
+
+const ShopMain = lazy(() => import(`../components/container/shop/ShopMainContainer`))
+const ShopFood = lazy(() => import(`../components/container/shop/ShopFoodContainer`))
+const ShopSnack = lazy(() => import(`../components/container/shop/ShopSnackContainer`))
+
+const ShopHouse = lazy(() => import(`../components/container/shop/ShopHouseContainer`))
+const ShopFashion = lazy(() => import(`../components/container/shop/ShopFashionContainer`))
+
+// 연준
+const ToyLayout = lazy(() => import('../components/container/shop/toy-yyj/ToyLayout'))
+const BathLayout = lazy(() => import('../components/container/shop/bath-yyj/BathLayout'))
+
+const toShopRouter = () => {
+    return (
+        [
+            {
+                // /shop,
+                path: '',
+                element: <Navigate replace to={'main'} />
+            },
+            {
+                path: 'main',
+                element: <Suspense fallback={Loading}><ShopMain /></Suspense>
+            },
+            {
+                // shop/food
+                path: 'food',
+                element: <Suspense fallback={Loading}><ShopFood /></Suspense>
+            },
+            {
+                // shop/snack
+                path: 'snack',
+                element: <Suspense fallback={Loading}><ShopSnack /></Suspense>
+            },
+            {
+                // shop/toy
+                path: 'toy',
+                element: <Suspense fallback={Loading}><ToyLayout /></Suspense>,
+                children: toToyRouter()
+            },
+            {
+                // shop/bath
+                path: 'bath',
+                element: <Suspense fallback={Loading}><BathLayout /></Suspense>,
+                children: toBathRouter()
+            },
+            {
+                // shop/toy
+                path: 'house',
+                element: <Suspense fallback={Loading}><ShopHouse /></Suspense>
+            },
+            {
+                // shop/toy
+                path: 'fashion',
+                element: <Suspense fallback={Loading}><ShopFashion /></Suspense>
+            },
+
+        ]
+    )
+}
+
+export default toShopRouter
