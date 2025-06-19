3e6e7e7bdeb5dc181d85e30ea0a4abd630d09de5
wjdrlgh93 <mario115@naver.com>
2025-06-19 13:10:03 +0900

    admin

--- src/components/container/admin/AdminMembers.jsx
+++ src/components/container/admin/AdminMembers.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useFormState } from 'react-dom'
+import { useNavigate } from 'react-router-dom'
 
 const AdminMembers = () => {
+
+    const [memberList, setMemberList] = useState([])
+    const navi = useNavigate()
+
+    useEffect(() => {
+        const onAdminMemberListFn = async () => {
+            const dataURL = `http://localhost:3000/members`
+            try {
+                const res = await axios.get(`${dataURL}`)
+                setMemberList(res.data)
+
+            } catch (err) {
+                alert(err)
+            }
+        }
+        onAdminMemberListFn()
+    }, [])
     return (
-        <div>AdminMembers</div>
+        // router Linking
+        <>
+            <div className="admin-memberList">
+                <div className="admin-memberList-con">
+                    <h1>회원목록</h1>
+                    <table>
+                        <tr>
+                            <th>ID</th>
+                            <th>userEmail</th>
+                            <th>userPw</th>
+                            <th>userName</th>
+                            <th>age</th>
+                            <th>address</th>
+                            <th>role</th>
+                            <th>보기</th>
+                        </tr>
+                        {memberList && memberList.map(el => {
+                            return (
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            )
+                        })}
+                    </table>
+                </div>
+            </div>
+        </>
     )
 }
 
