b7c98755ecdd731c88a3e5a88e17205b559b1b5a
wjdrlgh93 <mario115@naver.com>
2025-06-25 17:36:18 +0900

    test

--- src/components/container/shop/ShopBathContainer.jsx
+++ src/components/container/shop/ShopBathContainer.jsx
@@ -5,34 +5,35 @@ const ShopBathContainer = () => {
     const [bathList, setBathList] = useState()
 
     useEffect(() => {
-        const url = `http://192.168.23.209:3001/grooming`
+        // const url = `http://192.168.23.209:3001/grooming`
+        const url = `http://localhost:3001/grooming`
 
         fetch(url)
-        .then(res => res.json())
-        .then(jsonData => setBathList(jsonData))
+            .then(res => res.json())
+            .then(jsonData => setBathList(jsonData))
         // .catch(err => console.log(err))
-    },[])
-    
+    }, [])
+
     return (
-            <div className="toyList">
-                <ul>
-                {bathList && bathList.map((el)=>{
+        <div className="toyList">
+            <ul>
+                {bathList && bathList.map((el) => {
                     return (
                         <li>
-                            <Link to={`detail/${el.id}`}>                            
+                            <Link to={`detail/${el.id}`}>
                                 <div className="top">
                                     <img src={`/images/${el.img}`} alt={el.title} />
                                 </div>
                                 <div className="bottom">
                                     <span className="title">{el.title}</span>
                                     <span className="pricej">￦{el.price}</span>
-                                </div>                            
-                             </Link>
+                                </div>
+                            </Link>
                         </li>
                     )
                 })}
-                </ul>
-            </div>
+            </ul>
+        </div>
     )
 }
 
