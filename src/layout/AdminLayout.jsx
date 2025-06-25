aa9fc5fb7e6ed77bcb4040efdf0a0d557fa3f7ab
wjdrlgh93 <mario115@naver.com>
2025-06-25 17:14:49 +0900

    adminPage

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,21 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminHeader from '../components/common/Admin/AdminHeader'
 import AdminNav from '../components/common/Admin/AdminNav'
+import { Navigate, Outlet, useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useSelector } from 'react-redux'
 
-
-
-import { Outlet } from 'react-router-dom'
 
 const AdminLayout = () => {
+    const { id } = useParams();
+    const isLogin = useSelector(state => state.auth.isLogin)
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const isAuthenticated = isLogin || isLoggedIn;
+    const [memberObj, setMemberObj] = useState({}) // Object init
+
+    const navi = useNavigate();
+    const storedUserLoggedInformation = localStorage.getItem("isLoggedIn")
+
+
+    useEffect(() => {
+
+        const onAdminMemberListFn = async (memberId) => {
+            // get Member match by User
+            const dataURL = "http://localhost:3001/members"
+            try {
+
+                const res = await axios.get(`${dataURL}?id=${memberId}`)
+
+                setMemberObj(res.data[0])
+
+            } catch (err) {
+                alert(err)
+            }
+        }
+
+        onAdminMemberListFn()
+        console.log(setMemberObj)
+
+    }, [])
+
+    if (!storedUserLoggedInformation) {
+        return <Navigate to="/auth" replace />;
+    }
+
+
     return (
         <>
-            <div className="layoutRoot">
+
+            < div className="layoutRoot">
                 <AdminHeader />
                 <div className="layout-con">
                     <AdminNav />
                     <Outlet />
                 </div>
-            </div>
+            </div >
         </>
     )
 }
