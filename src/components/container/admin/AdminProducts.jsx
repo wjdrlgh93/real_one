e16dd8fe2c9fcc3672077b23b7612f33c61cc6d3
wjdrlgh93 <mario115@naver.com>
2025-07-01 11:57:58 +0900

    kakao

--- src/components/container/admin/AdminProducts.jsx
+++ src/components/container/admin/AdminProducts.jsx
@@ -152,6 +152,9 @@ const AdminProducts = () => {
                             )
                         })
                     }
+                    {
+
+                    }
 
                 </tbody>
             </table>
