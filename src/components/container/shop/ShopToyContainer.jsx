2c5409caee0e77c468c8d55c50b585de0eb46fd3
arichoddol <arichoddol@gmail.com>
2025-07-07 09:32:23 +0900

    0707-yyj임시

--- src/components/container/shop/ShopToyContainer.jsx
+++ src/components/container/shop/ShopToyContainer.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Paging from './layout-yyj/Paging'
+import { addCart } from '../../../slices/cartSlice'
 
 const ShopToyContainer = () => {
 
@@ -32,24 +33,42 @@ const ShopToyContainer = () => {
     const { currentPage, itemsPerPage } = useSelector(state => state.paging)
     const start = (currentPage - 1)*itemsPerPage
     const pagedItems = toyList.slice(start, start + itemsPerPage)
+    
+    const [count, setCount] = useState(1)
+    const [addCartModal, setAddCartModal] = useState(false)
+    const dispatch = useDispatch()
 
+    const addToCart = (item) => {
+        const { id, title, price, img, hoverImg } = item
+        const iconItem = {id, title, price, img, hoverImg, count:1}
+        dispatch(addCart(iconItem))
+        setAddCartModal(true)
+      }
+    
     return (
             <div className="toyList">
                 <ul>
                 {pagedItems.map((el)=>{
                     return (
                         <li key={el.id}>
-                            <Link to={`detail/${el.id}`}>                            
-                                <div className="top" onMouseEnter={el.hoverImg ? () => setIsHovered(el.id) : undefined} onMouseLeave={el.hoverImg ? () => setIsHovered(null) : undefined}>
-                                    <img src={el.hoverImg && isHovered === el.id ?`/images/${el.hoverImg}` : `/images/${el.img}`} alt={el.title}/>
+                            <div className="top" 
+                                onMouseEnter={el.hoverImg ? () => setIsHovered(el.id) : undefined} 
+                                onMouseLeave={el.hoverImg ? () => setIsHovered(null) : undefined}>
+                                <Link to={`detail/${el.id}`}>                            
+                                        <img 
+                                            src={el.hoverImg && isHovered === el.id ?`/images/${el.hoverImg}` : `/images/${el.img}`} 
+                                            alt={el.title}/>
+                                </Link>
+                                <div className="cartIcon" onClick={() => addToCart(el)} >
+                                    {/* <img src="/images/cart.png" alt="addToCart"/> */}┿
                                 </div>
-                            </Link>
-                                <div className="bottom">
+                            </div>
+                            <div className="bottom">
                                 <Link to={`detail/${el.id}`}>
                                     <span className="title">{el.title}</span>
                                 </Link>
-                                    <span className="pricej">￦{el.price}</span>
-                                </div>                            
+                                <span className="pricej">￦{el.price}</span>
+                            </div>                            
                         </li>
                     )
                 })}
